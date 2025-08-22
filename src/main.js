dc7880ec5a9d3bc71f2bb05164a6c0b07ebfbed5
shimacaze <work_staff@ymail.ne.jp>
2025-08-23 07:09:07 +0900

    学習用

---
+++ src/main.js
@@ -0,0 +1,6 @@
+const greet = document.getElementById('aisatsu');
+const changecolorBtn = document.getElementById('chcolor');
+
+changecolorBtn.addEventListener('click', function() {
+  greet.style.color = 'red';
+});
